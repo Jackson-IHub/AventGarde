876b561470820f6b39ae6c0400adc8e2741143aa
Isiaq Allison <ballerbajan@gmail.com>
2024-11-02 12:15:25 -0400

    Update playerController.cs

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class playerController : MonoBehaviour
 {
@@ -10,7 +11,14 @@ public class playerController : MonoBehaviour
 
         //tracks the current cell the player is occupying
         public GameObject currentCell;
-    
+
+        public CellManager cellManager;
+
+        public List<Vector2> possiblePositions;
+        private SpriteRenderer spriteRenderer;
+
+        //clear possible postitions do for loop for the resetting
+
 
         // will halt player movement when enemy are in motion
         // may not be needed if there is no animiations
@@ -21,6 +29,8 @@ public class playerController : MonoBehaviour
         currentCell = gridManager.grid[(int)playerPosition.x, (int)playerPosition.y];
         player.transform.SetParent(currentCell.transform, true);
         player.transform.localPosition = Vector2.zero; //visual update
+
+        //spriteRenderer = ManagerGetComponent<SpriteRenderer>();
         //ValidMoves();
 
         
@@ -31,7 +41,7 @@ public class playerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CheckLegalPositions();
     }
 
     // Update is called once per frame
@@ -46,35 +56,40 @@ public class playerController : MonoBehaviour
         //TODO needs to check for collision
         if (player.CompareTag("Player1") && !movementLock){
             if (Input.GetKeyUp(KeyCode.W)){
+                ClearPositions();
                 currentCell = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y+1];
                 player.transform.SetParent(currentCell.transform, true);
                 player.transform.localPosition = Vector2.zero;
                 playerPosition.y += 1;
-                //ValidMoves();
+                CheckLegalPositions();
             }
             if (Input.GetKeyUp(KeyCode.S)){
+                ClearPositions();
                 currentCell = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y-1];
                 player.transform.SetParent(currentCell.transform, true);
                 player.transform.localPosition = Vector2.zero;
                 playerPosition.y -= 1;
-                //ValidMoves();
+                CheckLegalPositions();
             }
             if (Input.GetKeyUp(KeyCode.A)){
+                ClearPositions();
                 currentCell = gridManager.grid[(int)playerPosition.x-1,(int)playerPosition.y];
                 player.transform.SetParent(currentCell.transform, true);
                 player.transform.localPosition = Vector2.zero;
                 playerPosition.x -= 1;
-                //ValidMoves();
+                CheckLegalPositions();
             }
             if (Input.GetKeyUp(KeyCode.D)){
+                ClearPositions();
                 currentCell = gridManager.grid[(int)playerPosition.x+1,(int)playerPosition.y];
                 player.transform.SetParent(currentCell.transform, true);
                 player.transform.localPosition = Vector2.zero;
                 playerPosition.x += 1;
-                //ValidMoves();
+                CheckLegalPositions();
             }
         }
         if (player.CompareTag("Player2") && !movementLock){
+            currentCell = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y];
             if (Input.GetKeyUp(KeyCode.UpArrow)){
                 currentCell = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y+1];
                 player.transform.SetParent(currentCell.transform, true);
@@ -103,6 +118,40 @@ public class playerController : MonoBehaviour
         
     }
 
+
+    private void CheckLegalPositions()
+    {
+
+        if(playerPosition.y - 1 >= 0) //can we go one down?
+        {
+            possiblePositions.Add(playerPosition - Vector2.up);
+            
+            gridManager.grid[(int)possiblePositions[possiblePositions.Count - 1].x, (int)possiblePositions[possiblePositions.Count - 1].y].GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
+        if(playerPosition.y + 1 <= 4) // can we go one up?
+        {
+            possiblePositions.Add(playerPosition + Vector2.up);
+        
+            gridManager.grid[(int)possiblePositions[possiblePositions.Count-1].x, (int)possiblePositions[possiblePositions.Count - 1].y].GetComponent<SpriteRenderer>().color = Color.yellow;
+            
+        }
+        if (playerPosition.x + 1 <= 4) // can we go right?
+        {
+            possiblePositions.Add(playerPosition + Vector2.right);
+
+            gridManager.grid[(int)possiblePositions[possiblePositions.Count - 1].x, (int)possiblePositions[possiblePositions.Count - 1].y].GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
+        if(playerPosition.x - 1 >= 0) //can we go left?
+        {
+            possiblePositions.Add(playerPosition - Vector2.right);
+
+            gridManager.grid[(int)possiblePositions[possiblePositions.Count - 1].x, (int)possiblePositions[possiblePositions.Count - 1].y].GetComponent<SpriteRenderer>().color = Color.yellow;
+            
+        }
+
+        
+    }
+
     public enum MoveDirection
     {
         UP, DOWN, LEFT, RIGHT
@@ -117,4 +166,12 @@ public class playerController : MonoBehaviour
 
     }
 
+
+    void  ClearPositions(){
+        for (int i = 0; i<possiblePositions.Count; i++){
+            
+            gridManager.grid[(int)possiblePositions[i].x, (int)possiblePositions[i].y].GetComponent<CellManager>().ResetColor();
+        }
+        possiblePositions.Clear();
+    }
 }
