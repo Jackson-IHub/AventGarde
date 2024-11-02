a4c8d6b574d3e713255144923e7610e0fb9b3894
Isiaq Allison <ballerbajan@gmail.com>
2024-11-02 11:20:27 -0400

    make the player movement native to the cell

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -1,4 +1,5 @@
 using JetBrains.Annotations;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
@@ -7,13 +8,24 @@ public class playerController : MonoBehaviour
         public Vector2 playerPosition;
         public GridManager gridManager;
 
+        //tracks the current cell the player is occupying
+        public GameObject currentCell;
+    
+
         // will halt player movement when enemy are in motion
         // may not be needed if there is no animiations
         public bool movementLock = false;
     void Awake(){
+        //Grab the current cell from manualy inputed player position
+
+        currentCell = gridManager.grid[(int)playerPosition.x, (int)playerPosition.y];
+        player.transform.SetParent(currentCell.transform, true);
+        player.transform.localPosition = Vector2.zero; //visual update
+        //ValidMoves();
+
         
         //place the player into the grid
-        gridManager.grid[(int)playerPosition.x,(int)playerPosition.y] = player;
+        //gridManager.grid[(int)playerPosition.x,(int)playerPosition.y] = player;
     }
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -26,6 +38,7 @@ public class playerController : MonoBehaviour
     void Update()
     {
         PlayerMove();
+        //ValidMoves();
     }
 
     void PlayerMove(){
@@ -33,41 +46,75 @@ public class playerController : MonoBehaviour
         //TODO needs to check for collision
         if (player.CompareTag("Player1") && !movementLock){
             if (Input.GetKeyUp(KeyCode.W)){
-                // Get the position of the grid in targeted direction and set players position to that position
-                player.transform.position = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y+1].transform.position;
+                currentCell = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y+1];
+                player.transform.SetParent(currentCell.transform, true);
+                player.transform.localPosition = Vector2.zero;
                 playerPosition.y += 1;
+                //ValidMoves();
             }
             if (Input.GetKeyUp(KeyCode.S)){
-                player.transform.position = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y-1].transform.position;
+                currentCell = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y-1];
+                player.transform.SetParent(currentCell.transform, true);
+                player.transform.localPosition = Vector2.zero;
                 playerPosition.y -= 1;
+                //ValidMoves();
             }
             if (Input.GetKeyUp(KeyCode.A)){
-                player.transform.position = gridManager.grid[(int)playerPosition.x-1,(int)playerPosition.y].transform.position;
+                currentCell = gridManager.grid[(int)playerPosition.x-1,(int)playerPosition.y];
+                player.transform.SetParent(currentCell.transform, true);
+                player.transform.localPosition = Vector2.zero;
                 playerPosition.x -= 1;
+                //ValidMoves();
             }
             if (Input.GetKeyUp(KeyCode.D)){
-                player.transform.position = gridManager.grid[(int)playerPosition.x+1,(int)playerPosition.y].transform.position;
+                currentCell = gridManager.grid[(int)playerPosition.x+1,(int)playerPosition.y];
+                player.transform.SetParent(currentCell.transform, true);
+                player.transform.localPosition = Vector2.zero;
                 playerPosition.x += 1;
+                //ValidMoves();
             }
         }
         if (player.CompareTag("Player2") && !movementLock){
             if (Input.GetKeyUp(KeyCode.UpArrow)){
-                // Get the position of the grid in targeted direction and set players position to that position
-                player.transform.position = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y+1].transform.position;
+                currentCell = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y+1];
+                player.transform.SetParent(currentCell.transform, true);
+                player.transform.localPosition = Vector2.zero;
                 playerPosition.y += 1;
             }
             if (Input.GetKeyUp(KeyCode.DownArrow)){
-                player.transform.position = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y-1].transform.position;
+                currentCell = gridManager.grid[(int)playerPosition.x,(int)playerPosition.y-1];
+                player.transform.SetParent(currentCell.transform, true);
+                player.transform.localPosition = Vector2.zero;
                 playerPosition.y -= 1;
             }
             if (Input.GetKeyUp(KeyCode.LeftArrow)){
-                player.transform.position = gridManager.grid[(int)playerPosition.x-1,(int)playerPosition.y].transform.position;
+                currentCell = gridManager.grid[(int)playerPosition.x-1,(int)playerPosition.y];
+                player.transform.SetParent(currentCell.transform, true);
+                player.transform.localPosition = Vector2.zero;
                 playerPosition.x -= 1;
             }
             if (Input.GetKeyUp(KeyCode.RightArrow)){
-                player.transform.position = gridManager.grid[(int)playerPosition.x+1,(int)playerPosition.y].transform.position;
+                currentCell = gridManager.grid[(int)playerPosition.x+1,(int)playerPosition.y];
+                player.transform.SetParent(currentCell.transform, true);
+                player.transform.localPosition = Vector2.zero;
                 playerPosition.x += 1;
             }
         }
+        
     }
+
+    public enum MoveDirection
+    {
+        UP, DOWN, LEFT, RIGHT
+    }
+
+    void ValidMoves(){
+        
+        gridManager.grid[(int)playerPosition.x,(int)playerPosition.y+1].GetComponent<SpriteRenderer>().color = Color.yellow;    
+        gridManager.grid[(int)playerPosition.x,(int)playerPosition.y-1].GetComponent<SpriteRenderer>().color = Color.yellow;
+        gridManager.grid[(int)playerPosition.x-1,(int)playerPosition.y].GetComponent<SpriteRenderer>().color = Color.yellow;
+        gridManager.grid[(int)playerPosition.x+1,(int)playerPosition.y].GetComponent<SpriteRenderer>().color = Color.yellow;
+
+    }
+
 }
